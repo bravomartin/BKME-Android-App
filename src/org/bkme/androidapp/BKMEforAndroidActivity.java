d51549e5229b106c96ca7dfbc2a30774ca392429
Martin Bravo <bravomartin@gmail.com>
2012-02-16 19:00:52 -0500

    intitial commit

---
+++ src/org/bkme/androidapp/BKMEforAndroidActivity.java
@@ -0,0 +1,13 @@
+package org.bkme.androidapp;
+
+import com.phonegap.*;
+import android.os.Bundle;
+
+public class BKMEforAndroidActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
